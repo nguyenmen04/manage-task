ebe31bffe62018f79d6c8ae2870c2137d934465c
Nguyen Thi Men <nguyenthimen190504@gmail.com>
2026-07-13 18:35:57 +0700

    chore(ci): enable cypress video recording and upload artifacts

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:5173',
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
